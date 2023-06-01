fec6fac31760cb771b4b8f7ea75c3d67b7acddc8
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-06-01 14:39:14 -0300

    primeira telinha

--- Aula04/fmjdfjmdf.js
+++ Aula04/fmjdfjmdf.js
@@ -4,19 +4,20 @@ const dataContainer = document.getElementById('data-container');
 button.addEventListener('click', function() {
     const number = box.value;
 
-    fetch(`http://localhost:8080/candidatos/id/${number}`, {
+    fetch(`http://localhost:8080/eleitores/titulo/${number}`, {
         method: 'GET'
     })
         .then(response => response.json())
         .then(data => {
-            const { nome, votos, partido } = data;
+            const { titulo, hasVoted } = data;
 
-            dataContainer.innerHTML = `
-              <h2>${nome}</h2>
-              <p>Votos: ${votos}</p>
-              <p>Partido: ${partido.name}</p>`;
+            dataContainer.innerHTML = `    
+              <p>Titulo: ${titulo}</p>
+              <p>Voto: ${hasVoted}</p>
+             `;
         })
         .catch(error => {
             console.log(error);
         });
 });
+
