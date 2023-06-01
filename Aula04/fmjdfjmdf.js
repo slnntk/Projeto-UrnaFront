fdd92a6d067250124952b15b304e515a1469c6a7
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-06-01 14:10:08 -0300

    progresso

---
+++ Aula04/fmjdfjmdf.js
@@ -0,0 +1,22 @@
+const button = document.getElementById('myButton');
+const box = document.getElementById('numberTitulo')
+const dataContainer = document.getElementById('data-container');
+button.addEventListener('click', function() {
+    const number = box.value;
+
+    fetch(`http://localhost:8080/candidatos/id/${number}`, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => {
+            const { nome, votos, partido } = data;
+
+            dataContainer.innerHTML = `
+              <h2>${nome}</h2>
+              <p>Votos: ${votos}</p>
+              <p>Partido: ${partido.name}</p>`;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
