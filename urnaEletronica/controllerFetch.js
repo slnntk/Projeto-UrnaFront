b442551aa9b78540ca936813190b80c56c5dd356
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-06-05 13:10:29 -0300

    Update controllerFetch.js

--- urnaEletronica/controllerFetch.js
+++ urnaEletronica/controllerFetch.js
@@ -16,7 +16,6 @@ button.addEventListener('click', function() {
               <p>Voto: ${hasVoted}</p>
              `;
         })
-
         .catch(error => {
             console.log(error);
         });
